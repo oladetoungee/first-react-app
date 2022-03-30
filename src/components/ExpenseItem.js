d6627b1e24de9eafdf62fba5c2104c197e23d993
oladetoungee <demilad1998@gmail.com>
2022-03-30 23:57:40 +0100

    date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
-
-    console.log(props)
-   console.log(props)
+    
+   
     return (
     <div className='expense-item'>
-        {/* <div>{props.date.toISOString()}</div> */}
+       <ExpenseDate date={props.date} />
         
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
