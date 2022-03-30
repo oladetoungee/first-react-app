539e09ca83467e73158a1705af208bfa6f50f7da
oladetoungee <demilad1998@gmail.com>
2022-03-30 14:40:46 +0100

    adding data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,19 @@
-function ExpenseItem() {
-    return <h2>ExpenseItem</h2>
-   
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+
+    console.log(props)
+   console.log(props)
+    return (
+    <div className='expense-item'>
+        {/* <div>{props.date.toISOString()}</div> */}
+        
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>N{props.amount}</div>
+        </div>
+    </div>
+    )
 }
 
 export default ExpenseItem;
