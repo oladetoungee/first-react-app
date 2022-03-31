2c1504856129fa150879b7a3914ca4e53e365809
oladetoungee <demilad1998@gmail.com>
2022-03-31 22:34:23 +0100

    I splitted my components into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,18 @@
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate'
+import "./ExpenseItem.css";
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    
-   
-    return (
-    <div className='expense-item'>
-       <ExpenseDate date={props.date} />
-        
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>N{props.amount}</div>
-        </div>
-    </div>
-    )
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">N{props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
